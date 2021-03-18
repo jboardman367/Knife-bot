ddd87e172bff367f50d064925bb634e42da89fff
jboardman367 <80042384+jboardman367@users.noreply.github.com>
2021-03-18 23:30:24 +1000

    Add empty structure and mostly functional PD

--- Knife/src/bot.py
+++ Knife/src/bot.py
@@ -1,12 +1,19 @@
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.messages.flat.QuickChatSelection import QuickChatSelection
 from rlbot.utils.structures.game_data_struct import GameTickPacket
+from rlbot.utils.game_state_util import GameState, BallState, CarState, Physics, Vector3, Rotator, GameInfoState, BoostState
 
 from util.ball_prediction_analysis import find_slice_at_time
 from util.boost_pad_tracker import BoostPadTracker
-from util.drive import steer_toward_target
 from util.sequence import Sequence, ControlStep
-from util.vec import Vec3
+from util.vec import Vec3, Line, Arc
+from util.package import Package
+from util.object_models import Car, Ball
+from random import randint
+import math
+
+#temporary
+from util.action import *
 
 
 class MyBot(BaseAgent):
@@ -15,6 +22,15 @@ class MyBot(BaseAgent):
         super().__init__(name, team, index)
         self.active_sequence: Sequence = None
         self.boost_pad_tracker = BoostPadTracker()
+        self.ball = None
+        self.me = None
+        self.allies = []
+        self.foes = []
+        self.ball_prediction = None
+        self.current_intent = None
+
+        #temporary
+        self.test_line = None
 
     def initialize_agent(self):
         # Set up information about the boost pads now that the game is active and the info is available
@@ -29,61 +45,140 @@ class MyBot(BaseAgent):
         # Keep our boost pad info updated with which pads are currently active
         self.boost_pad_tracker.update_boost_status(packet)
 
-        # This is good to keep at the beginning of get_output. It will allow you to continue
-        # any sequences that you may have started during a previous call to get_output.
-        if self.active_sequence is not None and not self.active_sequence.done:
-            controls = self.active_sequence.tick(packet)
-            if controls is not None:
-                return controls
+        #update cars and ball
+        self.update_objects(packet)
 
-        # Gather some information about our car and the ball
-        my_car = packet.game_cars[self.index]
-        car_location = Vec3(my_car.physics.location)
-        car_velocity = Vec3(my_car.physics.velocity)
-        ball_location = Vec3(packet.game_ball.physics.location)
+        # Create package of all useful info to be handed around as one object
+        package = Package(packet, self.me, self.ball, self.ball_prediction, self.allies, self.foes, self.renderer)
 
-        # By default we will chase the ball, but target_location can be changed later
-        target_location = ball_location
+        return self.blocking_debug(package)
 
-        if car_location.dist(ball_location) > 1500:
-            # We're far away from the ball, let's try to lead it a little bit
-            ball_prediction = self.get_ball_prediction_struct()  # This can predict bounces, etc
-            ball_in_future = find_slice_at_time(ball_prediction, packet.game_info.seconds_elapsed + 2)
+        #check current intent
+        if self.current_intent is not None:
+            if self.current_intent.check(package):
+                return self.current_intent.get_controls(package) #An existing and valid intent returns controls
 
-            # ball_in_future might be None if we don't have an adequate ball prediction right now, like during
-            # replays, so check it to avoid errors.
-            if ball_in_future is not None:
-                target_location = Vec3(ball_in_future.physics.location)
-                self.renderer.draw_line_3d(ball_location, target_location, self.renderer.cyan())
+        #At this point, self.current_intent is None or invalid, so find a new one
+        #This will be a decision consisting of 2 layers: car-ball-goal, other players
+        self.find_intent(package)
 
-        # Draw some things to help understand what the bot is thinking
-        self.renderer.draw_line_3d(car_location, target_location, self.renderer.white())
-        self.renderer.draw_string_3d(car_location, 1, 1, f'Speed: {car_velocity.length():.1f}', self.renderer.white())
-        self.renderer.draw_rect_3d(target_location, 8, 8, True, self.renderer.cyan(), centered=True)
+        #return the first controls of the new intent
+        if self.current_intent is not None:
+            return self.current_intent.get_controls(package)
 
-        if 750 < car_velocity.length() < 800:
-            # We'll do a front flip if the car is moving at a certain speed.
-            return self.begin_front_flip(packet)
+        #Make sure that the method always returns
+        return SimpleControllerState()
 
-        controls = SimpleControllerState()
-        controls.steer = steer_toward_target(my_car, target_location)
-        controls.throttle = 1.0
-        # You can set more controls if you want, like controls.boost.
 
-        return controls
 
-    def begin_front_flip(self, packet):
-        # Send some quickchat just for fun
-        self.send_quick_chat(team_only=False, quick_chat=QuickChatSelection.Information_IGotIt)
+    def update_objects(self, packet):
+        # ball
+        if self.ball is None:
+            self.ball = Ball(packet)
+        else:
+            self.ball.update(packet)
 
-        # Do a front flip. We will be committed to this for a few seconds and the bot will ignore other
-        # logic during that time because we are setting the active_sequence.
-        self.active_sequence = Sequence([
-            ControlStep(duration=0.05, controls=SimpleControllerState(jump=True)),
-            ControlStep(duration=0.05, controls=SimpleControllerState(jump=False)),
-            ControlStep(duration=0.2, controls=SimpleControllerState(jump=True, pitch=-1)),
-            ControlStep(duration=0.8, controls=SimpleControllerState()),
-        ])
+        self.ball_prediction = self.get_ball_prediction_struct()
+        # draw 3 sec of path
+        self.renderer.draw_polyline_3d(
+            [Vec3(ball_slice.physics.location) for ball_slice in self.ball_prediction.slices[:180:5]],
+            self.renderer.yellow())
 
-        # Return the controls associated with the beginning of the sequence so we can start right away.
-        return self.active_sequence.tick(packet)
+        # self
+        if self.me is None:
+            self.me = Car(self.index, packet)
+        elif self.me.index != self.index:
+            self.me = Car(self.index, packet)
+        else:
+            self.me.update(packet)
+
+        # check if number of players has changed, and reset allies and foes if it has
+        if len(self.allies) + len(self.foes) + 1 != len(packet.game_cars):
+            self.allies, self.foes = [], []
+
+        # allies
+        if len(self.allies) == 0:
+            for index in range(packet.num_cars):
+                if packet.game_cars[index].team == self.me.team and index != self.me.index:
+                    self.allies.append(Car(index, packet))
+        else:
+            for car in self.allies:
+                car.update(packet)
+
+        # foes
+        if len(self.foes) == 0:
+            for index in range(packet.num_cars):
+                if packet.game_cars[index].team != self.me.team:
+                    self.foes.append(Car(index, packet))
+        else:
+            for car in self.foes:
+                car.update(packet)
+
+    def blocking_debug(self, package) -> SimpleControllerState:
+        if self.test_line is None or package.packet.game_info.seconds_elapsed%3<=0.01:
+            self.test_line = Line(
+                Vec3(randint(-500,500),-4000,100),
+                Vec3(randint(-20,20),50,0)
+            )
+            self.set_game_state(
+                GameState(
+                    cars = {
+                        0:CarState(
+                            physics = Physics(
+                                location = Vector3(
+                                    x=0,
+                                    y=-4000,
+                                    z=17
+                                ),
+                                rotation = Rotator(
+                                    0,
+                                    math.pi/2,
+                                    0
+                                ),
+                                velocity = Vector3(
+                                    x=0,
+                                    y=1400,
+                                    z=0
+                                ),
+                                angular_velocity = Vector3(
+                                    x=0,
+                                    y=0,
+                                    z=0
+                                )
+                            )
+                        )
+                    },
+                    ball = BallState(
+                        Physics(
+                            velocity = Vector3(
+                                x=0,
+                                y=0,
+                                z=0
+                            ),
+                            location = Vector3(
+                                x=3000,
+                                y=-5000,
+                                z=98
+                            ),
+                            angular_velocity = Vector3(
+                                x=0,
+                                y=0,
+                                z=0
+                            )
+                        )
+                    )
+                )
+            )
+        #self.test_line = Line(
+        #    package.ball.location,
+        #    package.ball.location - package.me.location
+        #)
+        #positive side is orange
+        self.test_line.render(package.renderer)
+        return an_actual_pd(package, self.test_line, package.me, target_vel=1400)
+
+    def find_intent(self, package):
+        # gather variables to use in the decisions
+
+        # go through the tree
+        pass
